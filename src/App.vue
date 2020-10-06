b97b95d80315d28cfb9738d5896541c89dfe0d1b
shweezzy <naerok@icloud.com>
2020-10-06 16:58:26 +0300

    practice

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <forms :usersLogins="usersLogins" />
+    <h3>list of available users:</h3>
+    <div v-for="users in usersLogins" :key="users.login">
+      <ul style="padding-left: 0">
+        {{
+          users.login
+        }}
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import forms from "./components/forms";
+export default {
+  name: "App",
+  components: {
+    forms,
+  },
+  data() {
+    return {
+      usersLogins: [
+        {
+          login: "shweezzy",
+          passw: "1234",
+        },
+        {
+          login: "anastasia",
+          passw: "4321"
+        },
+        {
+          login: 'peter',
+          passw: "1111"
+        }
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/forms.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="labelsContainer">
+    <div>
+      <label>
+        Login
+        <input class="loginInput" type="text" v-model="login" />
+      </label>
+    </div>
+    <br />
+    <div>
+      <label>
+        Password
+        <input type="text" v-model="password" />
+      </label>
+    </div>
+    <br />
+    <button @click="onLogin" style="float: right">try it</button>
+    <div v-if="isAuthorized === true" class="succes-auth">
+      {{ this.login }} welcome back
+    </div>
+    <div v-if="isAuthorized === false" class="failed-auth">
+      Login or pass is wrong!!!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "forms",
+  props: {
+    usersLogins: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      login: null,
+      password: null,
+      isAuthorized: null,
+    };
+  },
+  methods: {
+    onLogin() {
+      const newUser = this.usersLogins.find(
+        (item) => item.login === this.login && item.passw === this.password
+      );
+      this.isAuthorized = !!newUser;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.failed-auth {
+  color: red;
+}
+.succes-auth {
+  color: green;
+}
+.labelsContainer {
+  width: 350px;
+  margin-left: 45%;
+}
+.loginInput {
+  margin-left: 9.7%;
+}
+input {
+  width: 72%;
+  margin-left: 3%;
+}
+button {
+  width: 20%;
+  height: 2em;
+  border: none;
+}
+</style>
